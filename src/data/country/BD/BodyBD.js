b7ceb89bb890d5df2249439fd50c17ab2baee048
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-01-19 21:54:01 +0600

    🛠️🏷️code cleanup

--- src/data/country/BD/BodyBD.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import BodyDhaka from "./Dhaka";
-
-export default function BodyBD({ language, state }) {
-  return <>{state === "dhaka" ? <BodyDhaka language={language} /> : ""}</>;
-}
--- src/data/timeKey/english.js
+++
@@ -1,9 +0,0 @@
-export const timeKeyEnglish = [
-  "Sehri Ends",
-  "Fajar",
-  "Sunrise",
-  "Dhuhr",
-  "Asr",
-  "Maghrib",
-  "Isha",
-];

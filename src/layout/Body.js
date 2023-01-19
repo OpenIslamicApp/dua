369d068d11ee2dcdbb8aa3d6fb487d308ee48928
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-01-19 21:54:40 +0600

    🛠️📕💅updated

--- src/layout/Body.js
+++ src/layout/Body.js
@@ -1,12 +1,10 @@
 import React from "react";
 import DUA from "./DUA";
-import BodyBD from "../data/country/BD/BodyBD";
 
 export default function Body({ language, country, state }) {
   return (
     <>
-      {country === "bd" ? <BodyBD language={language} state={state} /> : ""}
-      <h1 style={{ margin: "48px 0" }}>Dua's:</h1>
+      <h1 style={{ margin: "38px 0" }}>Dua's:</h1>
       <DUA />
     </>
   );
